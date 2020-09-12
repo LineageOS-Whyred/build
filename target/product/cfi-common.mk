95b1e6016518346fd34173809aa843b1511d531c
Adhitya Mohan <me@adhityamohan.in>
2020-09-12 12:20:38 +0530

    product: Add qcom-caf wpa_supplicant_8_lib to cfi path
    -- This can no longer be exported from vendor
    
    Change-Id: Ibea227442d23654298ed57f2e07be7f78b975a5f

--- target/product/cfi-common.mk
+++ target/product/cfi-common.mk
@@ -32,6 +32,7 @@ PRODUCT_CFI_INCLUDE_PATHS :=  \
     hardware/qcom/wlan/legacy/qcwcn/wpa_supplicant_8_lib \
     hardware/qcom/wlan/wcn6740/qcwcn/wpa_supplicant_8_lib \
     hardware/qcom/wlan/wcn7760/qcwcn/wpa_supplicant_8_lib \
+    hardware/qcom-caf/wlan/qcwcn/wpa_supplicant_8_lib \
     hardware/interfaces/keymaster \
     hardware/interfaces/security \
     packages/modules/Bluetooth/system \
